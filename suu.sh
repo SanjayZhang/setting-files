5d1422a896a1ec4702ab60bb3d318869351d0386
Zhang Shengjie <zhangsj_hit@outlook.com>
2020-02-29 09:25:49 +0800

    new

---
+++ suu.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+
+cd
+apt-get -y update
+apt-get -y upgrade
+
+check_and_install() {
+    for i in $@; do
+        if type $i >/dev/null 2>/dev/null; then
+            echo "$i already installed"
+        else
+            echo "$i does not exist, install it now"
+            apt-get install -y -q $i
+        fi
+    done
+}
+
+check_and_install curl
+
+# clone settings and tips
+check_and_install git
+git clone https://github.com/SanjayZhang/settings.git
+git clone https://github.com/SanjayZhang/tips.git
+chown -R $(logname):$(logname) settings
+chown -R $(logname):$(logname) tips
+
+# Vim, Vundle, Plugins
+check_and_install vim
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+vim +PluginInstall +qall
+
+
+# Tmux
+check_and_install_tmux
+git clone https://github.com/gpakosz/.tmux.git
+ln -s -f .tmux/.tmux.conf
+cp .tmux/.tmux.conf.local .
+
+cp settings/.tmux.conf.local .
+chown -R $(logname):$(logname) .tmux*
+
+
+# Zsh
+check_and_install zsh
+curl -Lo install.sh https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh
+
+# git
+cp settings/.gitconfig .
